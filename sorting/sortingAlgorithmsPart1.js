e3aead384caba470befa648483e9cba42fa837de
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-07-31 21:44:23 -0700

    finished insertionsort

--- sorting/sortingAlgorithmsPart1.js
+++ sorting/sortingAlgorithmsPart1.js
@@ -27,9 +27,25 @@ function selectionSort(arr){
         currentMinIndex = j;
       }
     }
-    [ arr[i], arr[currentMinIndex]] = [arr[currentMinIndex], arr[i]];
+    if(i !== currentMinIndex){
+      [ arr[i], arr[currentMinIndex]] = [arr[currentMinIndex], arr[i]];
+    }
   }
   return arr;
 }
 
 console.log(selectionSort([3.1, 0, 10, 4.5,1,2,5]));
+
+//Insertion Sort
+function insertionSort(arr) {
+  for(let i = 0; i < arr.length; i++){
+    let current = arr[i];
+    for(let j = i - 1; j > - 1 && arr[j] > current; j--){
+      arr[j+1] = [j];
+    }
+    arr[j+1] = current;
+  }
+  return arr;
+}
+
+//
